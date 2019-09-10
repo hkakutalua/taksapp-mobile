7be584682724988a490aed028c57beb5ac72d406
Henrick Kakutalua <henrykeys96@gmail.com>
2019-09-10 07:32:00 +0100

    First commit

---
+++ app/src/main/java/com/taksapp/taksapp/ui/WelcomeActivity.kt
@@ -0,0 +1,13 @@
+package com.taksapp.taksapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.taksapp.taksapp.R
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
